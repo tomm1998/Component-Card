a0c993a554e9df070ead556d5d081ea35360828d
Tommaso Ferrari <tommasoball@gmail.com>
2022-03-06 16:10:05 +0100

    almost css finished

--- src/App.js
+++ src/App.js
@@ -7,9 +7,19 @@ function App() {
   return (
     <div className="App">
       <Card
-        title='Card Title'
+        brand='CANNONDALE'
+        year='2019'
+        title='ADVENTURE NEO 1 EQ'
+        type='CORSA'
+        size='S M L XL'
+        colors='+4'
+        rating='5'
+        numReview='8560'
+        newOrOld='Usato'
+        priceDiscount='€3.299,00'
+        price='€3.299,00'
         imageUrl='https://lapierre-shopware.accell.cloud/thumbnail/76/f3/dc/1604483174/eSensium%20300%20D062%20(1)_800x800.png'
-        body='Prova' />
+        body='Una bici elettrica ideale per andare al lavoro o per godersi le caratteristiche stradine di campagna' />
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,72 @@
 import React from 'react'
 import './Card.css'
-function Card({ title, imageUrl, body }) {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight, faStar, faHeart } from '@fortawesome/free-solid-svg-icons'
+import Rating from 'react-rating'
+function Card({ title, year, size, priceDiscount, rating, newOrOld, numReview, price, colors, type, brand, imageUrl, body }) {
+    //da mettere a posto le icon delle stars e del cuore
+    //da fare lo slider delle immagini
     return (
         <div className='card-container'>
+            <div className='card-header'>
+                <div className='like'>
+                    <FontAwesomeIcon icon={faHeart}></FontAwesomeIcon>
+                </div>
+                <div className='newOrOld'>
+                    {newOrOld}
+                </div>
+            </div>
             <div className='image-container'>
-                <img src={imageUrl} alt='' />
+                <img src={imageUrl} alt='' className='image' />
             </div>
-            <div className='card-title'>
-                <h3>{title}</h3>
+            <div className='card-content'>
+                <div className='card-title'>
+                    <div className='card-brand'>
+                        <h3>{brand} | {year}</h3>
+                        <div className='rating'>
+                            <Rating
+                                initialRating={rating}
+                                emptySymbol={<FontAwesomeIcon icon={faStar}></FontAwesomeIcon>}
+                                fullSymbol={<FontAwesomeIcon icon={faStar}></FontAwesomeIcon>}
+                            />
+                            <span className='text-review'>{numReview}</span>
+                        </div>
+                    </div>
+                    <div>
+                        <h3>{title}</h3>
+                    </div>
+                </div>
+                <div className='card-body'>
+                    <div className='description'>
+                        <p>{body}</p>
+                    </div>
+                    <div className='card-type'>
+                        <p><span className='type-bike'>E-BIKE </span>{type}</p>
+                    </div>
+                    <div className='card-size-bike'>
+                        <p><span className='size-title'> TAGLIA </span> {size}</p>
+                    </div>
+                    <div className='card-colors-bike'>
+                        <p><span className='size-title'>COLORI</span>  {colors}</p>
+                    </div>
+                    <div className='card-price-bike'>
+                        <span>{priceDiscount}</span>
+                        <span className='price'>{price}</span>
+                    </div>
+                </div>
             </div>
-            <div className='card-body'>
-                <p>{body}</p>
-            </div>
-            <div className='btn'>
-                <button>
-                    <a>
-                        Scopri
-                    </a>
-                </button>
+            <div className='card-bottom'>
+                <div className='checkbox'>
+                    <label>
+                        <input type="checkbox" className='box' />
+                        COMPARA
+                    </label>
+                </div>
+                <div className=''>
+                    <button className='btn'>
+                        SCOPRI <FontAwesomeIcon icon={faArrowRight} />
+                    </button>
+                </div>
             </div>
         </div>
     )
